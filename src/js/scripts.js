0f0281868f49442e96085e856db889a4779cc1ab
dgavrichkov <dgavrichkov32@gmail.com>
2019-04-05 15:21:02 +0300

    добрался до телефонных размеров

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,6 @@
 const rangeSlider = document.querySelector('.range__slider');
 const rangeOutput = document.querySelector('.range__value');
-rangeOutput.innerHTML = rangeSlider.value + ' %';
+rangeOutput.value = rangeSlider.value + ' %';
 rangeSlider.oninput = function() {
-  rangeOutput.innerHTML = this.value + ' %';
+  rangeOutput.value = this.value + ' %';
 }

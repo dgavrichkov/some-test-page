b11f765075a4ca813ad9ccdd9282fe19945f0e39
DitmarGrim <dgavrichkov32@gmail.com>
2019-04-05 21:50:08 +0300

    поправил поехавшие строки в range

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,5 @@
 const rangeSlider = document.querySelector('.range__slider');
-const rangeOutput = document.querySelector('.range__value');
+const rangeOutput = document.querySelector('.range__value-input');
 rangeOutput.value = rangeSlider.value + ' %';
 rangeSlider.oninput = function() {
   rangeOutput.value = this.value + ' %';
